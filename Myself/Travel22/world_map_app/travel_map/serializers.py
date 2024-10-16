ecd618e7ccbdf1ad9e0513e279bb68e2ebad8532
Matthew <itsmat-github@gmx.com>
2024-10-16 18:01:18 +0100

    update

--- Myself/Travel22/world_map_app/travel_map/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import VisitedCountry
-
-class VisitedCountrySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VisitedCountry
-        fields = ['country_name']
